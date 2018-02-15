c977a8c373eba2b99b1b8db5d53d06df520d61c9
Sharon Funke <sharon.funke@nzz.ch>
2018-02-15 11:11:00 +0100

    Add english support

--- routes/routes.js
+++ routes/routes.js
@@ -2,5 +2,6 @@ module.exports = [
   require("./rendering-info/web.js"),
   require("./stylesheet.js"),
   require("./health.js"),
+  require("./locales.js"),
   require("./fixtures/data.js")
 ].concat(require("./schema.js"));
