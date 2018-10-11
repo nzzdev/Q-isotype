5a593a94e70118c3abc64c75d22442fb88c7efef
Philip Küng <kueng.philip@gmail.com>
2018-10-11 16:04:24 +0200

    added highlighting a category

---
+++ routes/option-availability.js
@@ -0,0 +1,23 @@
+const Boom = require("boom");
+const Joi = require("joi");
+
+module.exports = {
+  method: "POST",
+  path: "/option-availability/{optionName}",
+  options: {
+    validate: {
+      payload: Joi.object()
+    },
+    cors: true
+  },
+  handler: function(request, h) {
+    if (request.params.optionName === "highlightColumn") {
+      return {
+        available: true
+        //available: request.payload.data && request.payload.data.length >= 2
+      };
+    }
+
+    return Boom.badRequest();
+  }
+};
