1704150d4b1e7f97973b1af0a2a8a1ee8736fea5
Philip Küng <kueng.philip@gmail.com>
2019-06-11 12:02:26 +0200

    compare pngs and no-icons

--- views/templates/ComparableIsotype.svelte
+++ views/templates/ComparableIsotype.svelte
@@ -23,118 +23,41 @@
             class="q-isotype-comparable-column"
             class:q-isotype-first-column={colIndex === 0}>
             {#each newArray(val) as value, i}
-              <div
-                class="q-isotype-icon-container"
-                class:q-isotype-lowlight={isLowlight(rowIndex - 1)}
-                style="flex: 0 1 calc({iconContainerSize}% - 4px)">
+              {#if item.icons && item.icons[rowIndex - 1]}
                 <div
-                  class="q-isotype-icon-svg"
-                  style="{item.icons[rowIndex - 1].style};">
-                  <svg>
-                    <use xlink:href="#{item.icons[rowIndex - 1].key}" />
-                  </svg>
+                  class="q-isotype-icon-container"
+                  class:q-isotype-lowlight={isLowlight(rowIndex - 1)}
+                  style="flex: 0 1 calc({iconContainerSize}% - 4px)">
+                  {#if item.icons[rowIndex - 1].svg}
+                    <div
+                      class="q-isotype-icon-svg"
+                      style="{item.icons[rowIndex - 1].style};">
+                      <svg>
+                        <use xlink:href="#{item.icons[rowIndex - 1].key}" />
+                      </svg>
+                    </div>
+                  {:else}
+                    <div
+                      class="q-isotype-icon-png"
+                      style="background-image: url('{item.icons[rowIndex - 1].url}');" />
+                  {/if}
                 </div>
-              </div>
+              {:else}
+                <div
+                  class="q-isotype-icon-container"
+                  class:q-isotype-lowlight={isLowlight(rowIndex - 1)}
+                  style="flex: 0 1 calc({iconContainerSize}% - 4px)">
+                  <div class="q-isotype-icon-svg" style="width: 100%;">
+                    <svg>
+                      <use xlink:href="#no-icon-default-svg" />
+                    </svg>
+                  </div>
+                </div>
+              {/if}
             {/each}
           </div>
         {/if}
       {/each}
     </div>
   {/each}
-  <!-- {#each item.data.slice(1) as row, index}
-    <div class="q-isotype-row" style="width: calc(50% - 24px)">
-      <div class="q-isotype-row-title s-font-title-xs">{row[0]}</div>
-      {#if item.options && item.options.iconsOneRow}
-        <div class="q-isotype-icon-row">
-          {#each row as value, currentCategoryIndex}
-            {#if currentCategoryIndex > 0}
-              {#each newArray(row[currentCategoryIndex]) as value, i}
-                {#if item.icons && item.icons[currentCategoryIndex - 1]}
-                  <div
-                    class="q-isotype-icon-container"
-                    class:q-isotype-lowlight={isLowlight(currentCategoryIndex)}
-                    style="flex: 0 1 calc({iconContainerSize}% - 4px)">
-                    {#if item.icons[currentCategoryIndex - 1].svg}
-                      <div
-                        class="q-isotype-icon-svg"
-                        style="{item.icons[currentCategoryIndex - 1].style};">
-                        <svg>
-                          <use
-                            xlink:href="#{item.icons[currentCategoryIndex - 1].key}" />
-                        </svg>
-                      </div>
-                    {:else}
-                      <div
-                        class="q-isotype-icon-png"
-                        style="background-image: url('{item.icons[currentCategoryIndex - 1].url}');" />
-                    {/if}
-                  </div>
-                {:else}
-                  <div
-                    class="q-isotype-icon-container"
-                    class:q-isotype-lowlight={isLowlight(currentCategoryIndex)}
-                    style="flex: 0 1 calc({iconContainerSize}% - 4px)">
-                    <div class="q-isotype-icon-svg" style="width: 100%;">
-                      <svg>
-                        <use xlink:href="#no-icon-default-svg" />
-                      </svg>
-                    </div>
-                  </div>
-                {/if}
-              {/each}
-            {/if}
-          {/each}
-        </div>
-      {:else}
-        {#each row as value, currentCategoryIndex}
-          {#if currentCategoryIndex > 0}
-            <div
-              class="q-isotype-icon-row"
-              class:q-isotype-lowlight={isLowlight(currentCategoryIndex)}>
-              {#if categories.length === 1 && (row[currentCategoryIndex] == 0 || row[currentCategoryIndex] == null)}
-                <div
-                  class="q-isotype-icon-container"
-                  style="flex: 0 1 calc({iconContainerSize}% - 4px)">
-                  <div class="q-isotype-icon-png" />
-                </div>
-              {:else}
-                {#each newArray(row[currentCategoryIndex]) as i}
-                  {#if item.icons && item.icons[currentCategoryIndex - 1]}
-                    <div
-                      class="q-isotype-icon-container"
-                      style="flex: 0 1 calc({iconContainerSize}% - 4px)">
-                      {#if item.icons[currentCategoryIndex - 1].svg}
-                        <div
-                          class="q-isotype-icon-svg"
-                          style="{item.icons[currentCategoryIndex - 1].style};">
-                          <svg>
-                            <use
-                              xlink:href="#{item.icons[currentCategoryIndex - 1].key}" />
-                          </svg>
-                        </div>
-                      {:else}
-                        <div
-                          class="q-isotype-icon-png"
-                          style="background-image: url('{item.icons[currentCategoryIndex - 1].url}');" />
-                      {/if}
-                    </div>
-                  {:else}
-                    <div
-                      class="q-isotype-icon-container"
-                      style="flex: 0 1 calc({iconContainerSize}% - 4px)">
-                      <div class="q-isotype-icon-svg" style="width: 100%;">
-                        <svg>
-                          <use xlink:href="#no-icon-default-svg" />
-                        </svg>
-                      </div>
-                    </div>
-                  {/if}
-                {/each}
-              {/if}
-            </div>
-          {/if}
-        {/each}
-      {/if}
-    </div>
-  {/each} -->
 {/if}
