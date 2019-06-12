825060f59ea4b889a3bd5f22fe352f081a882af9
Philip Küng <kueng.philip@gmail.com>
2019-06-12 13:49:41 +0200

    outsource items on one row to component

--- views/templates/ComparableIsotype.svelte
+++ views/templates/ComparableIsotype.svelte
@@ -1,4 +1,5 @@
 <script>
+  import IconsOnOneRow from "./IconsOnOneRow.svelte";
   export let item;
   export let data;
   export let categories;
@@ -24,44 +25,12 @@
         <div
           class="q-isotype-icon-block"
           class:q-isotype-icon-block--first={rowIndex === 0}>
-          {#each row as value, currentCategoryIndex}
-            {#if currentCategoryIndex > 0}
-              {#each newArray(row[currentCategoryIndex]) as value, i}
-                {#if item.icons && item.icons[currentCategoryIndex - 1]}
-                  <div
-                    class="q-isotype-icon-container"
-                    class:q-isotype-lowlight={isLowlight(currentCategoryIndex)}
-                    style="flex: 0 1 calc({iconContainerSize}% - 4px)">
-                    {#if item.icons[currentCategoryIndex - 1].svg}
-                      <div
-                        class="q-isotype-icon-svg"
-                        style="{item.icons[currentCategoryIndex - 1].style};">
-                        <svg>
-                          <use
-                            xlink:href="#{item.icons[currentCategoryIndex - 1].key}" />
-                        </svg>
-                      </div>
-                    {:else}
-                      <div
-                        class="q-isotype-icon-png"
-                        style="background-image: url('{item.icons[currentCategoryIndex - 1].url}');" />
-                    {/if}
-                  </div>
-                {:else}
-                  <div
-                    class="q-isotype-icon-container"
-                    class:q-isotype-lowlight={isLowlight(currentCategoryIndex)}
-                    style="flex: 0 1 calc({iconContainerSize}% - 4px)">
-                    <div class="q-isotype-icon-svg" style="width: 100%;">
-                      <svg>
-                        <use xlink:href="#no-icon-default-svg" />
-                      </svg>
-                    </div>
-                  </div>
-                {/if}
-              {/each}
-            {/if}
-          {/each}
+          <IconsOnOneRow
+            {item}
+            {row}
+            {isLowlight}
+            {newArray}
+            {iconContainerSize} />
         </div>
       {/each}
     </div>
---
+++ views/templates/IconsOnOneRow.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let row;
+  export let item;
+  export let newArray;
+  export let isLowlight;
+  export let iconContainerSize;
+</script>
+
+{#each row as value, currentCategoryIndex}
+  {#each newArray(row[currentCategoryIndex]) as value, i}
+    {#if item.icons && item.icons[currentCategoryIndex - 1]}
+      <div
+        class="q-isotype-icon-container"
+        class:q-isotype-lowlight={isLowlight(currentCategoryIndex)}
+        style="flex: 0 1 calc({iconContainerSize}% - 4px)">
+        {#if item.icons[currentCategoryIndex - 1].svg}
+          <div
+            class="q-isotype-icon-svg"
+            style="{item.icons[currentCategoryIndex - 1].style};">
+            <svg>
+              <use xlink:href="#{item.icons[currentCategoryIndex - 1].key}" />
+            </svg>
+          </div>
+        {:else}
+          <div
+            class="q-isotype-icon-png"
+            style="background-image: url('{item.icons[currentCategoryIndex - 1].url}');" />
+        {/if}
+      </div>
+    {:else}
+      <div
+        class="q-isotype-icon-container"
+        class:q-isotype-lowlight={isLowlight(currentCategoryIndex)}
+        style="flex: 0 1 calc({iconContainerSize}% - 4px)">
+        <div class="q-isotype-icon-svg" style="width: 100%;">
+          <svg>
+            <use xlink:href="#no-icon-default-svg" />
+          </svg>
+        </div>
+      </div>
+    {/if}
+  {/each}
+{/each}
