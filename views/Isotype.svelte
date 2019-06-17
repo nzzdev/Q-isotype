ba34f87e751707ff30f8b211d6edfea433ee7dcb
Philip Küng <kueng.philip@gmail.com>
2019-06-17 15:54:08 +0200

    if more than 10 icons, show 10 icons per row in comparegroup-template

--- views/Isotype.svelte
+++ views/Isotype.svelte
@@ -16,7 +16,11 @@
       if (maxAmount < 5) {
         return 100 / maxAmount;
       } else {
-        return 20; // maxIconContainerSize will be 20% because there are at least 5 icons (100/5=20)
+        if (maxAmount > 10) {
+          return 10; // maxIconContainerSize will be 20% because there are at least 5 icons (100/5=20)
+        } else {
+          return 20;
+        }
       }
     } else {
       if (maxAmount < 10 || maxAmount >= 100) {
