0edc09cc69ea3fb19a714c4280d87d531ce9c102
Philip Küng <kueng.philip@gmail.com>
2019-06-25 13:43:15 +0200

    fix renderingInfoScript

--- helpers/renderingInfoScript.js
+++ helpers/renderingInfoScript.js
@@ -1,6 +1,34 @@
 function getMobileMinWidthScript(context) {
   const isotypeObject = `window.${context.id}`;
   return `
+    if (${isotypeObject} === undefined) {
+      ${isotypeObject} = {};
+    }
+    ${isotypeObject}.element = document.querySelector("#${context.id}");
+
+    ${isotypeObject}.handleMinWidth = function() {
+      var iconContainers = document.querySelectorAll('.q-isotype-icon-container');
+      if (${isotypeObject}.width > 400) {
+        ${isotypeObject}.removeMobileMinWidthClass(iconContainers);
+      } else {
+        ${isotypeObject}.addMobileMinWidthClass(iconContainers);
+      } 
+    };
+
+    ${isotypeObject}.removeMobileMinWidth = function(iconContainers) {
+      iconContainers.forEach(function(iconContainer){
+        if (iconContainer.classList.includes("-mobile")) {
+          iconContainer.classList.remove("q-isotype-icon-container" + "--" + iconContainer.dataset.containerType + "-mobile");
+        }
+      });
+    };
+
+    ${isotypeObject}.addMobileMinWidthClass = function(iconContainers) {
+      iconContainers.forEach(function(iconContainer){
+        iconContainer.classList.add("q-isotype-icon-container" + "--" + iconContainer.dataset.containerType + "-mobile");
+      });
+    };
+    
     if (!window.q_domready) {
       window.q_domready = new Promise(function(resolve) {
         if (document.readyState && (document.readyState === 'interactive' || document.readyState === 'complete')) {
@@ -34,34 +62,6 @@ function getMobileMinWidthScript(context) {
         }
       });
     }, 250));
-
-    if (${isotypeObject} === undefined) {
-      ${isotypeObject} = {};
-    }
-    ${isotypeObject}.element = document.querySelector("#${context.id}");
-
-    ${isotypeObject}.handleMinWidth = function() {
-      var iconContainers = document.querySelectorAll('.q-isotype-icon-container');
-      if (${isotypeObject}.width > 400) {
-        ${isotypeObject}.removeMobileMinWidthClass(iconContainers);
-      } else {
-        ${isotypeObject}.addMobileMinWidthClass(iconContainers);
-      } 
-    }
-
-    ${isotypeObject}.removeMobileMinWidth = function(iconContainers) {
-      iconContainers.forEach(function(iconContainer){
-        if (iconContainer.classList.includes("-mobile")) {
-          iconContainer.classList.remove("q-isotype-icon-container" + "--" + iconContainer.dataset.containerType + "-mobile");
-        }
-      });
-    }
-
-    ${isotypeObject}.addMobileMinWidthClass = function(iconContainers) {
-      iconContainers.forEach(function(iconContainer){
-        iconContainer.classList.add("q-isotype-icon-container" + "--" + iconContainer.dataset.containerType + "-mobile");
-      });
-    }
   `;
 }
 
